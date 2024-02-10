a20fd5f0bdf615d7cb8ab8e20cbed5b376f0b362
Matthew Sia <matthewsia98@gmail.com>
2024-02-10 17:49:32 -0500

    query api test

--- adspot-client/src/App.tsx
+++ adspot-client/src/App.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useQuery, gql } from "@apollo/client";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const query = gql`
+    query GetAuthors {
+      authors(order: [{ id: ASC }]) {
+        id
+        name
+        books {
+          id
+          title
+        }
+      }
+    }
+  `;
+  const { loading, error, data } = useQuery(query);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  if (loading) return <p>Loading ...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return data.authors.map(({ "id": authorId, "name": authorName, "books": authorBooks }) => (
+    <div key={authorId}>
+      <p>Author ID: {authorId}</p>
+      <p>Author Name: {authorName}</p>
+      <ul>
+        {authorBooks.map(({ "id": bookId, "title": bookTitle }) => (
+          <li key={bookId}>
+            <pre>Book ID:{bookId}    Title: {bookTitle}</pre>
+          </li>
+        ))}
+      </ul>
+      <br />
+    </div>
+  ));
 }
 
-export default App
+export default App;
--- adspot-client/src/main.tsx
+++ adspot-client/src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const client = new ApolloClient({
+  uri: "https://localhost:7031/",
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);

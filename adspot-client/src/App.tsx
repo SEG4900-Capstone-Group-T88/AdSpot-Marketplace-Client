9c211cfe125822bb024ab189802c8e5c0846b2be
Matthew Sia <matthewsia98@gmail.com>
2024-02-22 18:52:38 -0500

    get token but dont know how to use

--- adspot-client/src/App.tsx
+++ adspot-client/src/App.tsx
@@ -1,12 +1,29 @@
 import "./App.css";
 import AddUser from "./AddUser";
-import AuthorList from "./AuthorList";
+import LoginButton from "./LoginButton";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+    const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+    if (isAuthenticated) {
+        console.log(user);
+        getAccessTokenSilently().then((token) => {
+            console.log(token);
+            fetch("https://dev-r8salmbev4uhqcym.us.auth0.com/api/v2/users", {
+                method: "GET",
+                headers: {
+                    Accept: "application/json",
+                    Authorization: `Bearer ${token}`,
+                    "Content-Type": "application/json"
+                }
+            }).then((response) => console.log(response));
+        });
+    }
+
     return (
         <div>
+            <LoginButton />
             <AddUser />
-            <AuthorList />
         </div>
     );
 }
--- adspot-client/src/main.tsx
+++ adspot-client/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const client = new ApolloClient({
     uri: import.meta.env.VITE_GRAPHQL_API_ENDPOINT,
@@ -13,8 +14,16 @@ const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(
     <React.StrictMode>
-        <ApolloProvider client={client}>
-            <App />
-        </ApolloProvider>
+        <Auth0Provider
+            domain="dev-r8salmbev4uhqcym.us.auth0.com"
+            clientId="H7PEZ5RNCSTyHaTri9uSHxxrLlH1rkdg"
+            authorizationParams={{
+                redirect_uri: window.location.origin,
+            }}
+        >
+            <ApolloProvider client={client}>
+                <App />
+            </ApolloProvider>
+        </Auth0Provider>
     </React.StrictMode>,
 );
